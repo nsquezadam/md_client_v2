829f47109ec18d1a289d96c5c8e4a7b8ccc37093
Natalia Quezada M. <66387884+nsquezadam@users.noreply.github.com>
2024-11-18 20:05:45 -0300

    correccion archivos en static build

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -178,7 +178,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static_build/build']
+STATICFILES_DIRS = [BASE_DIR / 'static_build/']
 
 
 TEMPLATES[0]['DIRS'] = [BASE_DIR / 'static_build']
